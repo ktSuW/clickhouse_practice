d5028fb99320b215a59345142c5f1cbebdfdd218
su <suwindev19@gmail.com>
2024-05-15 22:01:34 +1000

    sql questions added

--- sql_hackerank/sql_hackerank.sql
+++ sql_hackerank/sql_hackerank.sql
@@ -1,4 +1,29 @@
 ```
+584.Find customer reference
+```
+select name from Customer
+where referee_id != 2 or referee_id is null;
+
+```
+1757. Recyclable and Low Fat Products
+```
+select product_id
+from products
+where low_fats='y' and recyclable='y';
+
+```
+1378. Replace Employee ID With The Unique Identifier
+```
+-- Employees table : id, name
+-- EmployeeUNI : id, unique_id
+-- Output - unique_id, name
+SELECT eu.unique_id, e.name 
+FROM Employees AS e
+LEFT JOIN EmployeeUNI AS eu
+ON e.id = eu.id;
+
+
+```
 Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 The CITY table is described as follows:
 ```
